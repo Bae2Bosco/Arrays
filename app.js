adad1e146d8c7cc4fda3dc88b44243e3ce5a570e
Bae2Bosco <89792104+Bae2Bosco@users.noreply.github.com>
2021-10-08 01:57:19 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -20,20 +20,25 @@ var warriors = [
         warrior: true
 
     }
-]
+];
 
-for(var i=0; i<warriors.length; i++){
-    var ele = document.createElement("div");
-    var nameEle = document.createElement("h2");
-    var damageEle = document.createElement("h4")
-    var healthEle = document.createElement("h6");
-    var warriorEle = document.createElement("h8");
-    nameEle.innerHTML=warriors[i].name;
-    damageEle.innerHTML=warriors[i].damage;
-    healthEle.innerHTML=warriors[i].health;
-    warriorEle.innerHTML=warriors[i].warrior;
-    wrapperEle.appendChild(ele);
-
+function contains(warriors, obj){
+    for(var i=0; i<warriors.length; i++) {
+        if (warriors[i] === obj) {
+            var ele = document.createElement("div");
+            var nameEle = document.createElement("h2");
+            var damageEle = document.createElement("h4")
+            var healthEle = document.createElement("h6");
+            var warriorEle = document.createElement("h8");
+            nameEle.innerHTML = warriors[i].name;
+            damageEle.innerHTML = warriors[i].damage;
+            healthEle.innerHTML = warriors[i].health;
+            warriorEle.innerHTML = warriors[i].warrior;
+            wrapperEle.appendChild(ele);
+            return true;
+        }
+    }
+    return false;
 }
 
 document.getElementById("myH2").style.color = "#ff0000";
